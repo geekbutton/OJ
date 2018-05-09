635e43c1f6787d0f71ae94af5754a1a907b7949a
geekbutton <panpeike1234@163.com>
2018-05-09 21:16:36 +0800

    442. 数组中重复的数据(*)

--- LeetCode/Array/442._数组中重复的数据.cpp
+++ LeetCode/Array/442._数组中重复的数据.cpp
@@ -12,10 +12,9 @@ public:
 	vector<int> findDuplicates(vector<int>& nums) {
 		vector<int> result;
 		for (int i = 0; i < nums.size(); ++i) {
-			int index = abs(nums[i]) - 1;
-			if (nums[index] < 0)
-				result.push_back(index + 1);
-			nums[index] = -nums[index];
+			if (nums[abs(nums[i]) - 1] < 0)
+				result.push_back(abs(nums[i]));
+			nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1];
 		}
 		return result;
 	}
