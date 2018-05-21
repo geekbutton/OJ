7224dc8f2b8ac0c043522bdc69a796bbf5deb331
geekbutton <panpeike1234@163.com>
2018-05-21 22:37:30 +0800

    347. 前K个高频元素

--- LeetCode/Heap/347_前K个高频元素.cpp
+++ LeetCode/Heap/347_前K个高频元素.cpp
@@ -7,33 +7,26 @@
 https://leetcode-cn.com/problems/top-k-frequent-elements/description/
 
 给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
 */
-/*整体思路是先求得数组中元素出现的频率，再将频率构成堆，从而可以获得前K个最高频率
+
+/*主要思路：利用堆排序获得前K个最大的元素，注意应使用无序map以保证时间复杂度
 */
 class Solution {
 public:
 	vector<int> topKFrequent(vector<int>& nums, int k) {
 		vector<int> result;
-		map<int, int> nm;
-		for (int i = 0; i < nums.size(); ++i) {						//利用map记录元素及其出现频率
-			++nm[nums[i]];
-		}
-		priority_queue<int> iq;
-		for (auto iter = nm.begin(); iter != nm.end(); ++iter) {	//利用频率建堆
-			iq.push(iter->second);
-		}
-		int num = nm.size();
-		while (iq.size() > num - k) {				//弹出前K个最大的频率
-			int value = iq.top();
+		unordered_map<int, int> im;
+		for (auto v : nums)
+			++im[v];
+		priority_queue<pair<int, int>> iq;
+		for (auto iter : im)
+			iq.push({ iter.second,iter.first });		//注意堆对应map元素交换
+		int n = 0;
+		while (n != k) {
+			result.push_back(iq.top().second);
 			iq.pop();
-			for (auto iter = nm.begin(); iter != nm.end(); ++iter) {
-				if (iter->second == value)
-				{
-					result.push_back(iter->first);
-					nm.erase(iter);				//注意删除已经选择的元素
-					break;
-				}
-			}
+			++n;
 		}
 		return result;
 	}
